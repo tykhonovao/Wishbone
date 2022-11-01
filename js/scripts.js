6d8b7e92b71e6b3d0e507b9a4c21102d702b70ad
tykhonovao <tykhonova.o.o@gmail.com>
2022-11-01 15:14:12 +0200

    add dialog

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,21 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            opacity: 0,
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
